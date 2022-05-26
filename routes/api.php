d1755bb305907cf2f1990eb823b6ffe7b6da4b89
fuadhasanemon <fuadhasanemon8@gmail.com>
2022-05-27 02:10:18 +0600

    Bug fixing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('todo', [TodoController::class, 'index']);
